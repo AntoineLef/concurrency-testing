489fc28c9f1ec381518835cb4b1f5142d4ffbbd4
Bruno Gagnon-Adam <bruno.gagnonadam@bell.ca>
2016-10-20 18:24:35 -0400

    Refactoring to have a more production ready implementation of the cache

--- src/main/java/ca/ulaval/glo4003/labo/concurrence/RealEstateCache.java
+++ src/main/java/ca/ulaval/glo4003/labo/concurrence/RealEstateCache.java
@@ -1,63 +1,67 @@
 package ca.ulaval.glo4003.labo.concurrence;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class RealEstateCache {
 
-	private Object lock = new Object();
-	private int refreshRateInMs;
+  private Object lock = new Object();
+  private int refreshRateInMs;
 
-	private Map<String, RealEstate> realEstates = new HashMap<>();
-	private RealEstateRepository realEstateRepository;
-	private ScheduledExecutorService executor;
+  private Map<String, RealEstate> realEstates = new ConcurrentHashMap<>();
+  private RealEstateRepository realEstateRepository;
+  private ScheduledExecutorService executor;
 
-	public RealEstateCache(RealEstateRepository realEstateRepository,
-			int RefreshRateInMs) {
-		this.realEstateRepository = realEstateRepository;
-		refreshRateInMs = RefreshRateInMs;
+  public RealEstateCache(RealEstateRepository realEstateRepository, int RefreshRateInMs) {
+    this.realEstateRepository = realEstateRepository;
+    refreshRateInMs = RefreshRateInMs;
+  }
 
-		start();
-	}
+  public RealEstate getRealEstate(String id) {
+    RealEstate realEstate = realEstates.get(id);
+    if (realEstate == null) {
+      synchronized (lock) {
+        realEstate = realEstates.get(id);
+        if (realEstate == null) {
+          realEstate = realEstateRepository.findById(id);
+          realEstates.put(id, realEstate);
+          System.out.println(Thread.currentThread().getName() + " Feed cache");
+          return realEstate;
+        }
+        System.out.println(Thread.currentThread().getName() + " Cache already feed");
+        return realEstate;
+      }
+    }
+    System.out.println(Thread.currentThread().getName() + " Found in cache");
+    return realEstate;
+  }
 
-	public RealEstate getRealEstate(String id) {
-		RealEstate realEstate = realEstates.get(id);
-		if (realEstate == null) {
-			synchronized (lock) {
-				realEstate = realEstates.get(id);
-				if (realEstate == null) {
-					System.out.println(Thread.currentThread().getName()
-							+ " Feeding cache");
-					realEstate = realEstateRepository.findById(id);
-					realEstates.put(id, realEstate);
-					return realEstate;
-				}
-				System.out.println(Thread.currentThread().getName()
-						+ " Cache already feed");
-				return realEstate;
-			}
-		}
-		System.out
-				.println(Thread.currentThread().getName() + " Found in cache");
-		return realEstate;
-	}
+  public synchronized void start() {
+    if (executor == null) {
+      executor = Executors.newScheduledThreadPool(1);
+    }
+    Runnable clearCache = new Runnable() {
+      public void run() {
+        clearCache();
+        System.out.println(Thread.currentThread().getName() + " Cleared cache");
+      }
 
-	public void start() {
-		executor = Executors.newScheduledThreadPool(1);
-		Runnable clearCache = new Runnable() {
-			public void run() {
-				realEstates.clear();
-			}
-		};
+    };
 
-		executor.scheduleAtFixedRate(clearCache, 0, refreshRateInMs,
-				TimeUnit.MILLISECONDS);
-	}
+    executor.scheduleAtFixedRate(clearCache, 0, refreshRateInMs, TimeUnit.MILLISECONDS);
+  }
 
-	public void stop() {
-		executor.shutdown();
-	}
+  public synchronized void stop() {
+    executor.shutdown();
+  }
+
+  public void clearCache() {
+    synchronized (lock) {
+      realEstates.clear();
+    }
+  }
+
 }
--- src/test/java/ca/ulaval/glo4003/labo/concurrence/RealEstateCacheTest.java
+++ src/test/java/ca/ulaval/glo4003/labo/concurrence/RealEstateCacheTest.java
@@ -18,99 +18,93 @@ import edu.umd.cs.mtc.TestFramework;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RealEstateCacheTest {
-	private static final String REAL_ESTATE_ID = "uLaval";
-	private static final int REFRESH_RATE = 50;
+  private static final String REAL_ESTATE_ID = "uLaval";
+  private static final int REFRESH_RATE = 500;
+  private static final int RUN_COUNT = 200;
 
-	@Mock
-	private RealEstateRepository realEstateRepository;
-	@Mock
-	private RealEstate realEstate;
+  @Mock
+  private RealEstateRepository realEstateRepository;
+  @Mock
+  private RealEstate realEstate;
 
-	private RealEstateCache realEstateCache;
+  private RealEstateCache realEstateCache;
 
-	@Before
-	public void setUp() {
-		realEstateCache = new RealEstateCache(realEstateRepository,
-				REFRESH_RATE);
-		given(realEstateRepository.findById(REAL_ESTATE_ID)).willReturn(
-				realEstate);
-	}
+  @Before
+  public void setUp() {
+    realEstateCache = new RealEstateCache(realEstateRepository, REFRESH_RATE);
+    given(realEstateRepository.findById(REAL_ESTATE_ID)).willReturn(realEstate);
+  }
 
-	@Test
-	public void givenRealEstateNotInCache_whenFindById_thenFetchInRepository()
-			throws Exception {
-		// given
+  @Test
+  public void givenRealEstateNotInCache_whenFindById_thenFetchInRepository() throws Exception {
+    // given
 
-		// when
-		RealEstate realEstateFromCache = realEstateCache
-				.getRealEstate(REAL_ESTATE_ID);
+    // when
+    RealEstate realEstateFromCache = realEstateCache.getRealEstate(REAL_ESTATE_ID);
 
-		// then
-		Mockito.verify(realEstateRepository, times(1)).findById(REAL_ESTATE_ID);
-		assertThat(realEstateFromCache, is(realEstate));
-	}
+    // then
+    Mockito.verify(realEstateRepository, times(1)).findById(REAL_ESTATE_ID);
+    assertThat(realEstateFromCache, is(realEstate));
+  }
 
-	@Test
-	public void givenRealEstateInCache_whenFindById_thenUseCache()
-			throws Exception {
-		// given
-		realEstateCache.getRealEstate(REAL_ESTATE_ID);
+  @Test
+  public void givenRealEstateInCache_whenFindById_thenUseCache() throws Exception {
+    // given
+    realEstateCache.getRealEstate(REAL_ESTATE_ID);
 
-		// when
-		RealEstate realEstateFromCache = realEstateCache
-				.getRealEstate(REAL_ESTATE_ID);
+    // when
+    RealEstate realEstateFromCache = realEstateCache.getRealEstate(REAL_ESTATE_ID);
 
-		// then
-		Mockito.verify(realEstateRepository, times(1)).findById(REAL_ESTATE_ID);
-		assertThat(realEstateFromCache, is(realEstate));
-	}
+    // then
+    Mockito.verify(realEstateRepository, times(1)).findById(REAL_ESTATE_ID);
+    assertThat(realEstateFromCache, is(realEstate));
+  }
 
-	@Test
-	public void givenThreeThreadsAtTheSameTime_whenGetRealEstate_thenFetchOneTimeInRepository()
-			throws Throwable {
-		TestFramework
-				.runManyTimes(
-						new GivenThreeThreadsAtTheSameTime_whenGetRealEstate_thenFetchOneTimeInRepository(),
-						200);
-	}
+  @Test
+  public void givenMultipleThreadsAtTheSameTime_whenGetRealEstate_thenFetchOneTimeInRepository()
+    throws Throwable
+  {
+    TestFramework.runManyTimes(new GivenMultipleThreadsAtTheSameTime_whenGetRealEstate_thenFetchOneTimeInRepository(),
+                               RUN_COUNT);
+  }
 
-	class GivenThreeThreadsAtTheSameTime_whenGetRealEstate_thenFetchOneTimeInRepository
-			extends MultithreadedTestCase {
-		@Override
-		public void initialize() {
-			realEstateCache = new RealEstateCache(realEstateRepository,
-					REFRESH_RATE);
-			reset(realEstateRepository);
-			given(realEstateRepository.findById(REAL_ESTATE_ID)).willReturn(
-					realEstate);
-			System.out.println("------");
-		}
+  class GivenMultipleThreadsAtTheSameTime_whenGetRealEstate_thenFetchOneTimeInRepository
+    extends
+      MultithreadedTestCase
+  {
+    @Override
+    public void initialize() {
+      reset(realEstateRepository);
+      given(realEstateRepository.findById(REAL_ESTATE_ID)).willReturn(realEstate);
+      realEstateCache = new RealEstateCache(realEstateRepository, REFRESH_RATE);
+      realEstateCache.start();
+      System.out.println("------");
+    }
 
-		public void thread1() {
-			getRealEstate();
-		}
+    public void thread1() {
+      getRealEstate();
+    }
 
-		public void thread2() {
-			getRealEstate();
-		}
+    public void thread2() {
+      getRealEstate();
+    }
 
-		public void thread3() {
-			getRealEstate();
-		}
+    public void thread3() {
+      getRealEstate();
+    }
 
-		private void getRealEstate() {
-			waitForTick(1);
-			System.out.println(Thread.currentThread().getName()
-					+ " Getting real estate");
-			RealEstate realEstateInCache = realEstateCache
-					.getRealEstate(REAL_ESTATE_ID);
+    private void getRealEstate() {
+      waitForTick(1);
+      System.out.println(Thread.currentThread().getName() + " Getting real estate");
+      RealEstate realEstateInCache = realEstateCache.getRealEstate(REAL_ESTATE_ID);
 
-			assertThat(realEstateInCache, is(realEstate));
-		}
+      assertThat(realEstateInCache, is(realEstate));
+    }
 
-		@Override
-		public void finish() {
-			verify(realEstateRepository, atMost(1)).findById(anyString());
-		}
-	}
+    @Override
+    public void finish() {
+      realEstateCache.stop();
+      verify(realEstateRepository, atMost(1)).findById(anyString());
+    }
+  }
 }
